a4ee655f2b379a27a8c2ea3559820cb09d3d725a
Rajat Agrawal <rajatagrawal9410@gmail.com>
2023-05-16 10:13:57 +0530

    Changes In snow_integration

--- snow_integration/files/cpu_util.ps1
+++ snow_integration/files/cpu_util.ps1
@@ -1,9 +1,10 @@
- $thresholdValue = 3    
- $Processor = (Get-WmiObject -ComputerName localhost -Class win32_processor -ErrorAction Stop | Measure-Object -Property LoadPercentage -Average | Select-Object Average).Average  
- if ($Processor -le $thresholdValue){
-    Write-Host "CPU is under threshold value"
+ $Processor = (Get-WmiObject -ComputerName localhost -Class win32_processor -ErrorAction Stop | Measure-Object -Property LoadPercentage -Average | Select-Object Average).Average 
+
+ if (78-ge 75 ){
+   Write-Host "Server CPU usage is above threshold"
+   $topfive = Get-Process | Sort-Object CPU -desc | Select-Object -first 5  
+   $topfive
     }
  else{        
-    $topfive = Get-Process | Sort-Object CPU -desc | Select -first 5
-    $topfive   
+   Write-Host "Server CPU usage is in under threshold"  
     } 
